34ebe3be0a8c99c347174c7b41581731403ffd81
Lu Shiqi <lsshiqi.22@ichat.sp.edu.sg>
2023-10-23 09:29:55 +0800

    ex1

---
+++ bmi.py
@@ -0,0 +1,20 @@
+def calculate_bmi(height, weight):
+    print("Height = " + str(height))
+    print("Weight = " + str(weight))
+
+    # Calculate BMI
+    bmi = weight / (height * height)
+
+    # Display calculated BMI
+    print("BMI = {:.2f}".format(bmi))
+
+    # Determine weight classification
+    if bmi < 18.5:
+        print("Weight Classification: Under Weight")
+    elif 18.5 <= bmi <= 25.0:
+        print("Weight Classification: Normal Weight")
+    else:
+        print("Weight Classification: Over Weight")
+
+
+calculate_bmi(weight=57, height=1.73)
