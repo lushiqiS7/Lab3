5184e88ed27acb986118ef5d693b013399f6630e
Lu Shiqi <lsshiqi.22@ichat.sp.edu.sg>
2023-10-29 20:35:21 +0800

    PyTest for TestLab3 Table3

--- Lab3.py
+++ Lab3.py
@@ -1,3 +1,4 @@
+
 print("Lab 3 - Software Unit Testing with PyTest")
 
 SORT_ASCENDING = 0
@@ -5,36 +6,34 @@ SORT_DESCENDING = 1
 
 
 def bubble_sort(arr, sorting_order):
-
     # Copy input list to results list
     arr_result = arr.copy()
 
     # Get number of elements in the list
     n = len(arr_result)
 
-    if n < 10:
-        # Traverse through all array elements
-        for i in range(n - 1):
-            # range(n) also work but outer loop will
-            # repeat one time more than needed.
+    if n > 10:
+        return 1  # Return 1 as per REQ-03
 
-            # Last i elements are already in place
-            for j in range(0, n - i - 1):
+    # Handle cases with less than or equal to 10 numbers
+    if n == 0:
+        return 0  # Return 0 as per REQ-04
 
-                if sorting_order == SORT_ASCENDING:
-                    if arr_result[j] > arr_result[j + 1]:
-                        arr_result[j], arr_result[j + 1] = arr_result[j + 1], arr_result[j]
+    if not all(isinstance(item, int) for item in arr_result):
+        return 2  # Return 2 as per REQ-05
 
+    if sorting_order not in [SORT_ASCENDING, SORT_DESCENDING]:
+        return []  # Return an empty list for invalid sorting order
 
-                elif sorting_order == SORT_DESCENDING:
-                    if arr_result[j] < arr_result[j + 1]:
-                        arr_result[j], arr_result[j + 1] = arr_result[j + 1], arr_result[j]
-
-                else:
-                    # Return an empty array
-                    arr_result = []
-    else:
-        arr_result = -1
+    # Sort the list
+    for i in range(n - 1):
+        for j in range(0, n - i - 1):
+            if sorting_order == SORT_ASCENDING:
+                if arr_result[j] > arr_result[j + 1]:
+                    arr_result[j], arr_result[j + 1] = arr_result[j + 1], arr_result[j]
+            elif sorting_order == SORT_DESCENDING:
+                if arr_result[j] < arr_result[j + 1]:
+                    arr_result[j], arr_result[j + 1] = arr_result[j + 1], arr_result[j]
 
     return arr_result
 
